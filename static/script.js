e679f25af8a3542c70066f6291b1e6f0cfc0dd54
Atitebi Ayoola Emmanuel <123402047+Ayoola-Emmanuel@users.noreply.github.com>
2023-08-03 16:40:33 +0100

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $(".navbar").toggleClass("nav-toggle");
+  });
+
+  $(window).on("load scroll", function () {
+    $("#menu").removeClass("fa-times");
+    $(".navbar").removeClass("nav-toggle");
+  });
+});
