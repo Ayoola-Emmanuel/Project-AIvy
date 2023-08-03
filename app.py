9eeee51bfb25ba1c70be3ba307abef4448425ffc
Atitebi Ayoola Emmanuel <123402047+Ayoola-Emmanuel@users.noreply.github.com>
2023-08-03 16:36:13 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+
+from chat import get_response
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/chat")
+def chat_get():
+    return render_template("base.html")
+
+@app.get("/chat")
+def index_get():
+    return render_template("base.html")
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    #TODO: check if text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run()
